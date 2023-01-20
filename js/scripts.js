1fe0736413c1931307a696e697196f5391569ca2
Mesha <madsketchz@gmail.com>
2023-01-20 12:53:59 -0800

    Add UI Elements, removed unnecessary repitition and seperation. WIP getting results to display.

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,36 @@ function beepBoop(number) {
   let number = [];
 
   for (let i = 0; i <= number; i++) {
-    if (i === 1) {
+    if (i.toString().includes(1) && !i.toString(2) && !i.toString(3)) {
       let i = "Beep!";
       numbers.push(i);
-    } else if (i === 2) {
+    } else if (i.toString().includes(2) && !i.toString(3)) {
       let i = "Boop!";
       numbers.push(i);
-    } else if (i === 3) {
+    } else if (i.toString().includes(3)) {
       let i = "Won't you be my neighbor?";
       numbers.push(i);
     } else {
       numbers.push(i);
     };
   }
+  return numbers;
 }
 
 // ui logic
-
 window.onload = function() {
-  document.querySelector("form#number-form").onsubmit = function(event) {
+  document.querySelector("number-form").onsubmit = function(event) {
     event.preventDefault();
+    const div = document.getElementById("u-array");
+    const number1 = document.getElementById("number1").ariaValueMax;
+    let p = document.createElement("display-array");
+    let numberArray = beepBoop(number1)
+    p.innerText = numberArray.join(", ");
   };
-};
+
+  function reset() {
+    document.getElementById("number-form").reset();
+    const p = document.getElementById("display-array");
+    p.innerText = "";
+  }
+}
